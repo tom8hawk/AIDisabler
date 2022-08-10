36e03bc6d98e9cc57bf019caa874280346ae0ede
tom8hawk <grinly.ilpogo@gmail.com>
2022-08-10 07:03:53 +0300

    Оптимизация

--- src/main/java/ru/baronessdev/personal/aidisabler/AIDisabler.java
+++ src/main/java/ru/baronessdev/personal/aidisabler/AIDisabler.java
@@ -1,18 +1,23 @@
 package ru.baronessdev.personal.aidisabler;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public final class AIDisabler extends JavaPlugin {
     public static AIDisabler inst;
+    private static final Map<UUID, List<LivingEntity>> previous = new HashMap<>();
 
     public AIDisabler() {
         inst = this;
@@ -26,8 +31,8 @@ public final class AIDisabler extends JavaPlugin {
                 .map(EntityType::valueOf)
                 .collect(Collectors.toList());
 
-        Bukkit.getScheduler().runTaskTimer(this, () -> Bukkit.getWorlds().forEach(world -> world.getLivingEntities().stream()
-                .filter(ent -> blocked.contains(ent.getType())).forEach(ent -> ent.setAI(false))), 0L, 20L);
+        Bukkit.getWorlds().forEach(world -> world.getLivingEntities().stream()
+                        .filter(ent -> blocked.contains(ent.getType())).forEach(ent -> ent.setAI(false)));
 
         Bukkit.getPluginManager().registerEvents(new Listener() {
 
@@ -39,6 +44,34 @@ public final class AIDisabler extends JavaPlugin {
                 }
             }
 
+            @EventHandler
+            public void onMove(PlayerMoveEvent event) {
+                Location toLoc = event.getTo();
+
+                if (toLoc != null) {
+                    Block toBlock = toLoc.getBlock();
+                    Block fromBlock = event.getFrom().getBlock();
+
+                    if (!toBlock.equals(fromBlock)) {
+                        UUID uuid = event.getPlayer().getUniqueId();
+
+                        if (previous.containsKey(uuid))
+                            previous.get(uuid).forEach(living -> living.setAI(false));
+
+                        List<LivingEntity> now = new ArrayList<>();
+                        toBlock.getWorld().getNearbyEntities(toBlock.getLocation(), 15, 15, 15, ent -> blocked.contains(ent.getType())).forEach(entity -> {
+                            if (entity instanceof LivingEntity) {
+                                LivingEntity living = (LivingEntity) entity;
+                                living.setAI(true);
+
+                                now.add(living);
+                            }
+                        });
+
+                        previous.put(uuid, now);
+                    }
+                }
+            }
         }, this);
     }
 
